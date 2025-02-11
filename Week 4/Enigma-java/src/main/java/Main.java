4d1e40812714515de44688c1c6253194d1820b69
Jayden <L00184357@atu.ie>
2025-02-11 17:07:43 +0000

    Created Start Message

--- Week 4/Enigma-java/src/main/java/Main.java
+++ Week 4/Enigma-java/src/main/java/Main.java
@@ -1,14 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)
     {
+        Scanner keyboardIn = new Scanner(System.in);
         // Obtain a reference to the console
         Console con = System.console();
 
-        System.out.println("Hello world");
+        System.out.println("Enter Message:");
 
         // List of rotors to map characters. Spaces in the rotor strings should be ignored.
         List<String> rotors = new ArrayList<String>();
@@ -17,9 +19,9 @@ public class Main {
         rotors.add("EKMFLGDQVZNTOWYHXUSPAIBRCJ");
 
         // ToDO
-        // Get a start message, encrypt r3gw4hg4wqgqgq4 gqegqit and then decrypt it
+        // Get a start message, encrypt it and then decrypt it
 
-        String startMessage = "Hello world";
+        String startMessage = keyboardIn.nextLine();
         String encryptedMessage = Enigma.Encrypt(startMessage, 4, rotors);
         System.out.println("The encrypted message is: " + encryptedMessage);
 
